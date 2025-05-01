55f818e2730b4488ce5678508ae9f9d92e85216c
Pedram Karimi <noonegandom@hotmail.com>
2025-05-01 13:51:20 +0330

    fix

--- WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/Form1.Designer.cs
@@ -31,31 +31,113 @@
             folderBrowserDialog1 = new FolderBrowserDialog();
             textBox1 = new TextBox();
             button1 = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            label1 = new Label();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            richTextBox1 = new RichTextBox();
+            label3 = new Label();
+            label4 = new Label();
+            linkLabel1 = new LinkLabel();
             SuspendLayout();
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(16, 12);
+            textBox1.Location = new Point(88, 15);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(271, 23);
+            textBox1.Size = new Size(209, 23);
             textBox1.TabIndex = 0;
             textBox1.Click += textBox1_Click;
             // 
             // button1
             // 
-            button1.Location = new Point(212, 41);
+            button1.Location = new Point(184, 44);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(113, 23);
             button1.TabIndex = 1;
-            button1.Text = "button1";
+            button1.Text = "Fix SVG names";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(70, 15);
+            label1.TabIndex = 2;
+            label1.Text = "SVG Folder :";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(265, 143);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(441, 23);
+            textBox2.TabIndex = 3;
+            textBox2.Click += textBox2_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 146);
+            label2.Name = "label2";
+            label2.Size = new Size(247, 15);
+            label2.TabIndex = 4;
+            label2.Text = "After add new sprit to current sprit click here :";
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(265, 172);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(441, 248);
+            richTextBox1.TabIndex = 5;
+            richTextBox1.Text = "";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(190, 175);
+            label3.Name = "label3";
+            label3.Size = new Size(63, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Duplicate :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 120);
+            label4.Name = "label4";
+            label4.Size = new Size(139, 15);
+            label4.TabIndex = 4;
+            label4.Text = "Now generate sprit with :";
+            // 
+            // linkLabel1
+            // 
+            linkLabel1.AutoSize = true;
+            linkLabel1.Location = new Point(157, 120);
+            linkLabel1.Name = "linkLabel1";
+            linkLabel1.Size = new Size(107, 15);
+            linkLabel1.TabIndex = 6;
+            linkLabel1.TabStop = true;
+            linkLabel1.Text = "https://svgsprit.es/";
+            linkLabel1.LinkClicked += linkLabel1_LinkClicked;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(linkLabel1);
+            Controls.Add(richTextBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(label1);
             Controls.Add(button1);
             Controls.Add(textBox1);
             Name = "Form1";
@@ -69,5 +151,13 @@
         private FolderBrowserDialog folderBrowserDialog1;
         private TextBox textBox1;
         private Button button1;
+        private OpenFileDialog openFileDialog1;
+        private Label label1;
+        private TextBox textBox2;
+        private Label label2;
+        private RichTextBox richTextBox1;
+        private Label label3;
+        private Label label4;
+        private LinkLabel linkLabel1;
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -1,3 +1,6 @@
+using System.Text;
+using System.Text.RegularExpressions;
+
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
@@ -5,11 +8,6 @@ namespace WinFormsApp1
         public Form1()
         {
             InitializeComponent();
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
         }
 
         private void textBox1_Click(object sender, EventArgs e)
@@ -45,6 +43,41 @@ namespace WinFormsApp1
             else
             {
                 Console.WriteLine("Directory does not exist.");
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://svgsprit.es/");
+        }
+
+        private void textBox2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = openFileDialog1.FileName;
+                var source_path = textBox2.Text;
+
+                var idList = new List<string>();
+
+                var source_file = File.ReadAllText(source_path);
+                var matches = Regex.Matches(source_file, @"(?<=id="")[a-z-0-9]+(?="")", RegexOptions.IgnoreCase | RegexOptions.CultureInvariant | RegexOptions.Compiled);
+                foreach (Match match in matches)
+                {
+                    idList.Add(match.Value);
+                }
+
+                var duplicates = idList.GroupBy(s => s)
+                                .Where(g => g.Count() > 1)
+                                .Select(g => g.Key);
+
+                var sb = new StringBuilder();
+                foreach (var duplicate in duplicates)
+                {
+                    sb.AppendLine(duplicate);
+                }
+
+                richTextBox1.Text = sb.ToString();
             }
         }
     }
